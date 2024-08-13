8d0629d7fc9bd67cf64c8dcbee8a367276bbb817
vvquangit <vuquangit@gmail.com>
2024-08-13 11:47:02 +0700

    feat: import svg file

--- src/react-native-config.d.ts
+++ src/react-native-config.d.ts
@@ -4,8 +4,18 @@ declare module 'react-native-config' {
     API_URL: string
     FAKE_EMAIL?: string
     FAKE_PASSWORD?: string
+    GOOGLE_CLIENT_ID_WEB: string
+    GOOGLE_CLIENT_ID_IOS: string
+    GOOGLE_REVERSED_CLIENT_ID_IOS: string
   }
 
   export const Config: NativeConfig
   export default Config
+}
+
+declare module '*.svg' {
+  import React from 'react'
+  import {SvgProps} from 'react-native-svg'
+  const content: React.FC<SvgProps>
+  export default content
 }
